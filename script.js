5375c8ef7b2c49e1a118fceb9f43f4618d09e45a
Ajay Dhital <azaydhital1@gmail.com>
2024-09-06 00:00:01 +0545

    fff

--- script.js
+++ script.js
@@ -78,3 +78,17 @@ Array.from(document.getElementsByClassName('songItemPlay')).forEach((element)=>{
         e.target.classList.add('fa-pause-circle');
     })
 })
+document.getElementById('next').addEventListener('click',()=>{
+    if(songIndex>=9){
+        songIndex=0;
+    }
+    else{
+        songIndex+=1;
+    }
+    audioElement.src=`songs/${songIndex+1}.mp3`;
+    masterSongName.innerText=songs[songIndex].songName;
+    audioElement.currentTime=0;
+    audioElement.play();
+    masterPlay.classList.remove('fa-play-circle');
+    masterPlay.classList.add('fa-pause-circle');
+})
